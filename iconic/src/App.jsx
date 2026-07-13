69dae7730a4a878a528934fcb82e3315c6e6eb85
Flurry <merpcutemr@gmail.com>
2026-07-13 12:40:25 -0400

    Add React router

--- iconic/src/App.jsx
+++ iconic/src/App.jsx
@@ -1,17 +1,13 @@
+import { Route, Routes } from 'react-router'
 import './App.css'
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <div className='h-screen py-0 my-0 flex items-center max-w-150 mx-auto'>
-        <div className='my-auto flex flex-col w-full'>
-          <h1 className='text-7xl mb-10 animate-pan-l'>Welcome.</h1>
-          <div className='text-right animate-pan-r'>
-            <button className='secondary text-2xl!'>Open from file</button>
-            <button className='primary text-2xl!'>Create an icon</button>
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
